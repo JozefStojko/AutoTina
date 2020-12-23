0106183f5edca46e5373fa841b6343c39b1e4a85
JozefStojko <sztojko.jozsef@gmail.com>
2020-12-23 16:43:05 +0100

    set input validation for Years

--- Angular7Client/src/app/admin-products/create-car-type/create-car-type.component.ts
+++ Angular7Client/src/app/admin-products/create-car-type/create-car-type.component.ts
@@ -13,9 +13,12 @@ import { CarTypeService } from 'src/app/shared/service/car-type.service';
 export class CreateCarTypeComponent implements OnInit {
   
   godinaProizvodnjeOdValidna: boolean = true;
+  godinaProizvodnjeOdValidnaDisabled: boolean = true;
   godinaProizvodnjeDoValidna: boolean = true;
   allCarMarks: CarMark[];
   carMarkId: number = null;
+  currentYear: number=new Date().getFullYear();
+  godinaPocetkaProizvodnje: number = new Date().getFullYear();;
 
 
 
@@ -44,5 +47,42 @@ export class CreateCarTypeComponent implements OnInit {
   this.carMarkId = +this.carMarkId;
   console.log(this.carMarkId);
 }
+
+onKeyYearFrom(event: any) { // without type info
+  if (1900 < event.target.value && event.target.value <= this.currentYear){
+    this.godinaProizvodnjeOdValidna = false;
+    this.godinaProizvodnjeOdValidnaDisabled = true;
+  }
+  else {
+    this.godinaProizvodnjeOdValidna = true;
+    this.godinaProizvodnjeOdValidnaDisabled = false;
+  }
+  this.godinaPocetkaProizvodnje = event.target.value
+  console.log(event.target.value);
+}
+
+onKeyYearTo(event: any) { // without type info
+  if (this.godinaPocetkaProizvodnje <= event.target.value && event.target.value <= this.currentYear){
+    this.godinaProizvodnjeDoValidna = false;
+  }
+  else {
+    this.godinaProizvodnjeDoValidna = true;
+  }
+  console.log(event.target.value);
+}
+
+stripTextyearTo(event) {
+  const seperator  = '^([0-9])';
+  const maskSeperator =  new RegExp(seperator , 'g');  
+  let result =maskSeperator.test(event.key);   return result;
+   }
+
+   stripTextyearFrom(event) {
+    const seperator  = '^([0-9])';
+    const maskSeperator =  new RegExp(seperator , 'g');  
+    let result =maskSeperator.test(event.key);   return result;
+     }
+  
+   
 
 }
